a97140b8f34c2622c3a86336a10c2e2f2ca5e4a0
pacot2 <pacot2@nycstudents.net>
2024-11-21 09:25:04 -0500

    POII=ASDOIFOIASDIJOFOIJASDOIJFIJO;AWE;OIJF;OIJASDOIJ;FOA;IJSDFOIJASOIJDFOIJ;ASDOIJFOIJ
    A;OISJDJ;FAS;IOJFOASIDJFAOISDJFASIJF;AOSIDJF;ASIDJF;ASOIDJF;AOISDJF;OADAIJSD;OFIAJSE;DFIKJA;SEDIFKJA;SEOIFJA;SEIOFJA;OEIFJA;OSIEDFJ;ASEFJ;SIOFJA;SOEIFJ;ASEIKJF;SEODIAFEAFFAOI;ESA;OIFESDFOISEOIFES;AEOIFOIJAOIJ;AEFSOIJOIJAFESOI;EFSDO;IJSEAFJIOEJIO;

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import time
 
 testVar = int(input("Test "))
 choiceVar = int(input("1 or 2? "))
@@ -8,7 +9,9 @@ if choiceVar == 1:
 else:
     testVar += 2
 
+MAX = 30
 root = Tk()
+progressVar = DoubleVar()
 frm = ttk.Frame(root, padding=400)
 frm.grid()
 style = ttk.Style()
@@ -17,8 +20,14 @@ ttk.Label(frm, text="臭膣屄").grid(column=0, row=3)
 ttk.Button(frm, text="懾屎跤", command=root.destroy).grid(column=0, row=5)
 ttk.Label(frm, text="Test", style="BW.TLabel", width="100").grid(column=0, row=8)
 ttk.Combobox(frm, justify="center", values=['test 1', 'test 2', 'test 3', '4', '5', '6', '7', '8', '9', '0'], state="readonly").grid(column=0, row=9)
-
-p = ttk.Progressbar()
-p.step()
-ttk.Progressbar(frm, orient="horizontal", length="500", value=testVar, mode="determinate", variable=testVar).grid(column=0, row=10)
+progressbar = ttk.Progressbar(root, variable=progressVar, maximum=MAX)
+def loop_function():
+    k = 0
+    while k <= MAX:
+    ### some work to be done
+        progressVar.set(k)
+        k += 1
+        time.sleep(0.02)
+        root.update_idletasks()
+    root.after(100, loop_function)
 root.mainloop()
